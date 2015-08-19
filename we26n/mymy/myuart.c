3e8c635581c82fbb6697f708ea232504c4e1b219
wmd <mingdong.wang@janluyou.com>
2015-08-19 15:09:29 +0800

    modify ble uart baud to 115200

--- we26n/mymy/myuart.c
+++ we26n/mymy/myuart.c
@@ -49,7 +49,7 @@ int  my_uart_init( void )
   USART_Init( USART2, &USART_InitStructure);
 	
 	/**/
-  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_BaudRate = 115200;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = USART_StopBits_1;
   USART_InitStructure.USART_Parity = USART_Parity_No;
